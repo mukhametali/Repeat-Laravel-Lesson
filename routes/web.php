1c41173c9278cca9915dc5c3b3d845d3f9561fbc
mukhametali <m.kussaiynov1996@gmail.com>
2023-05-21 18:09:59 +0600

    Lesson type 2

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'ali';
+    return view('welcome');
 });
-
-Route::get('/posts', 'PostController@index');
-Route::get('/posts/create', 'PostController@create');
-Route::get('/posts/update', 'PostController@update');
-Route::get('/posts/delete', 'PostController@delete');
-Route::get('/posts/first_or_create', 'PostController@firstOrCreate');
-Route::get('/posts/update_or_create', 'PostController@updateOrCreate');
