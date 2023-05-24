1cc3bf41cd696829adaaaed9da21cc703b8b0a1a
mukhametali <m.kussaiynov1996@gmail.com>
2023-05-24 10:10:39 +0600

    Blade Directives.@if, @isset, @production, @env, @for, @foreach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,42 @@
         <div class="container">
             {!! mb_strtoupper($h1) !!}
             {{-- $title  --}}
+
+            {{--@if(count($data1) > 20)
+                Count > 20
+            @elseif(count($data1) < 20)
+                Count < 20
+            @else
+                Count = 20
+            @endif--}}
+
+            {{--@isset($data3)
+                Isset data2
+            @endisset--}}
+
+            {{--@production
+                <h1>PRODUCTION</h1>
+            @endproduction--}}
+
+            {{--@env('local')
+                <h1>Local</h1>
+            @endenv--}}
+
+            {{--@for($i =0; $i < count($data1); $i++)
+                @if($data1[$i] % 2 != 0)
+                    @continue
+                @elseif($data1[$i] == 6 || $data1[$i] == 8)
+                    @continue
+                @elseif($data1[$i] == 16)
+                    @break
+                @endif
+                {{ $data1[$i] }}
+            @endfor--}}
+
+            {{--@foreach($data2 as $k => $v)
+                {{ $k  }} => {{ $v }} <br>
+            @endforeach--}}
+
         </div>
     </section>
 
