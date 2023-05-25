183f90dfa30ce3ff98009a13f33bc724f8499409
mukhametali <m.kussaiynov1996@gmail.com>
2023-05-25 15:19:14 +0600

    Mail class. Test send mail in mailtrap.io

---
+++ resources/views/send.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.layout')
+
+@section('title')
+
+@endsection
+
+@section('header')
+    @parent
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="alert-alert-success">
+            Письмо отправлено!
+        </div>
+    </div>
+@endsection
